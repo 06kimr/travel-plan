0ffccb00cb8e51d0d16a6ffc7798ee548d100089
rickykim-99 <youngs.kim@creadto.com>
2024-12-31 21:13:10 +0900

    [FEAT] google map api added

--- client/src/pages/plan/City.tsx
+++ client/src/pages/plan/City.tsx
@@ -1,18 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import WideLayout from "../../components/common/WideLayout";
+import Map from "../../components/plan/Map";
 import PlanController from "../../components/plan/PlanController";
 import TravelPeriodModal from "../../components/plan/TravelPeriodModal";
 import { usePlanStore } from "../../store";
+import { useParams } from "react-router-dom";
+import { getCity } from "../../services/plan";
+import Loading from "../../components/common/Loading";
 
 export default function PlanCity() {
   const { status } = usePlanStore();
+  const {city: cityId = ""} = useParams();
+  const {data, isLoading} = useQuery({
+    queryKey: ['city', cityId],
+    queryFn: () => getCity(cityId),
+  })
   return (
     <>
       {status === "period_edit" && <TravelPeriodModal />}
       <WideLayout>
+        {isLoading || !data ? <Loading />: (
         <div className="flex h-full">
           <PlanController />
-          <div className="flex-1 bg-gray300">지도 영역</div>
+          <div className="flex-1 bg-gray300">
+            <Map center={data.coordinates} />
+          </div>
         </div>
+        )}
       </WideLayout>
     </>
   );
